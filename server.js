8dec2226fd2410eb4f8eec52e80a8651981c975a
himashree56 <himashree966@gmail.com>
2025-04-03 18:24:00 +0530

    cors updated

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+// app.use(cors());/
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
